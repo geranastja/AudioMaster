ef0f67af2b9df42d15eb25cb6c5c776c2fff3859
Anastasiya Iskakova <geranastja@gmail.com>
2023-05-23 16:29:25 +0600

    Added tests

--- BoxTests/Tests.cs
+++ BoxTests/Tests.cs
@@ -16,7 +16,7 @@ namespace BoxTests
             string EditUser = "EditUserAutoTest";
             driver.Navigate().GoToUrl(baseURL+ "cabinet/users");
             Authorization(site, AdminLogin);
-            Message = "Search through a filter the user " + User;
+            Message = "2. Search through a filter the user " + User;
             WaitUntilVisible(By.XPath(".//*[@role='rowgroup']/tr[1]"));
             WaitUntilVisibleAndClick(By.XPath(".//*[@id='search-select']"));//filter
             WaitUntilVisibleAndClick(By.XPath(".//*[@id='search-select']/option[@value='username']"));//select filter
@@ -30,7 +30,7 @@ namespace BoxTests
             //если есть юзер, то удаляем
             if (IsElementPresent(By.XPath(".//a[contains(text(),'" + User + "')]")) == true)
             {
-                Message = "   Deleting " + User;
+                Message = "  2.1 Deleting " + User;
                 driver.FindElement(By.XPath(".//*[@role='cell']//*[contains(@class,'menu-dots')]")).Click();
                 WaitUntilVisibleAndClick(By.XPath(".//*[@role='cell']//*[contains(text(),'Delete')]"));
                 WaitUntilVisibleAndClick(By.XPath(".//*[contains(@class,'shadow-modal')]//*[contains(text(),'Delete')]"));
@@ -47,8 +47,8 @@ namespace BoxTests
                 WaitUntilInVisible(By.XPath(".//a[contains(text(),'" + EditUser + "')]"));
                 Console.Write(Message);
             }
-            Console.WriteLine("Adding the new user " + User);
-            Message = "  Field Validation";
+            Console.WriteLine("3. Adding the new user " + User);
+            Message = "  3.1 Field Validation";
             WaitUntilVisibleAndClick(By.XPath(".//button[contains(text(),'Add user')]"));
             WaitUntilVisible(By.XPath(".//*[@type='submit']"));
             driver.FindElement(By.XPath(".//*[@id='username']")).SendKeys("1");
@@ -69,7 +69,7 @@ namespace BoxTests
             WaitUntilVisible(By.XPath(".//*[@for='Manual']/following-sibling::span[contains(@class,'error')]"));
             Console.WriteLine(Message);
 
-            Message = "  Entering data into fields ";
+            Message = "  3.2 Entering data into fields ";
             driver.FindElement(By.XPath(".//*[@id='username']")).Clear();
             driver.FindElement(By.XPath(".//*[@id='username']")).SendKeys(User);
             driver.FindElement(By.XPath(".//*[@id='password']")).SendKeys(User);
@@ -92,7 +92,7 @@ namespace BoxTests
             Console.WriteLine(Message);
 
             //edit the user
-            Message = "User editing";
+            Message = "4. User editing";
             driver.FindElement(By.XPath(".//*[@role='cell']//*[contains(@class,'menu-dots')]")).Click();
             WaitUntilVisibleAndClick(By.XPath(".//*[@role='cell']//*[contains(text(),'Update')]"));
             WaitUntilVisible(By.XPath(".//*[@id='username'][@value='UserAutoTest']"));
@@ -112,7 +112,7 @@ namespace BoxTests
             Console.WriteLine(Message);
 
             //deleting the user
-            Message = "Deleting user " + EditUser;
+            Message = "5. Deleting user " + EditUser;
             driver.FindElement(By.XPath(".//*[@role='cell']//*[contains(@class,'menu-dots')]")).Click();
             WaitUntilVisibleAndClick(By.XPath(".//*[@role='cell']//*[contains(text(),'Delete')]"));
             WaitUntilVisibleAndClick(By.XPath(".//*[contains(@class,'shadow-modal')]//*[contains(text(),'Delete')]"));
@@ -243,7 +243,7 @@ namespace BoxTests
             driver.Navigate().GoToUrl(baseURL + "cabinet/moderate");
             WaitUntilVisible(By.XPath("(.//*[@role='rowgroup']//a)[contains(text(),'" + recordId + "')]"));
             Console.WriteLine(Message);
-
+       
         }
 
 
@@ -270,17 +270,142 @@ namespace BoxTests
             Console.WriteLine(Message + Record1 + ", " + Record2);
             Message = "4. Check info on Page of glue record ";
              WaitUntilVisible(By.XPath(".//*[contains(text(),'Record is glued')]"));
-            WaitUntilInVisible(By.XPath(".//*[@class='wrapper-preloader']/div"));//preloDER isnt
             string GluRecord = driver.FindElement(By.XPath(".//*[@class='text-xl font-semibold uppercase']")).Text;
+            Message += GluRecord;
+            WaitUntilInVisible(By.XPath(".//*[@class='wrapper-preloader']/div"));//preloDER isnt
             WaitUntilVisible(By.XPath(".//*[contains(text(),'"+ Record1 + ","+ Record2 + "')]"));
-            Console.WriteLine(Message + GluRecord);
-            Message = "5. Click 'Back'. Check the absence of the glue record and the presence of the old records";
+            Console.WriteLine(Message);
+            Message = "5. Click 'Back'";
             driver.FindElement(By.XPath(".//*[contains(text(),'Back')]")).Click();
+            Console.WriteLine(Message);
+            Message = "  5.1 Check the absence of the glue record and the presence of the old records";
             WaitUntilVisible(By.XPath("(.//*[@role='rowgroup']//a)[contains(text(),'"+ GluRecord + "')]"));
+            Console.WriteLine(Message);
+            Message = "  5.2 Check the presence of the old records";
             WaitUntilInVisible(By.XPath("(.//*[@role='rowgroup']//a)[contains(text(),'" + Record2 + "')]"));
             WaitUntilInVisible(By.XPath("(.//*[@role='rowgroup']//a)[contains(text(),'" + Record1 + "')]"));
             Console.WriteLine(Message);
+        
         }
+
+
+        [Test]
+        [TestCaseSource(nameof(GetSites))]
+        public void DeleteRecord(string site)
+        {
+            driver.Navigate().GoToUrl(baseURL + "cabinet/records");
+            Authorization(site, ModerateLogin);
+            Message = "2. Page of Records";
+            WaitUntilVisible(By.XPath("(.//*[@role='rowgroup']//a)[1]"));
+            string Record1 = driver.FindElement(By.XPath("(.//*[@role='rowgroup']//a)[1]")).Text;
+            string Record2 = driver.FindElement(By.XPath("(.//*[@role='rowgroup']//tr[2]//a)[1]")).Text;
+            Console.WriteLine(Message);
+
+            Message = "3. Delete record in the Records list ";
+            driver.FindElement(By.XPath(".//a[contains(text(),'" + Record1 + "')]" +
+                "//ancestor::tr//*[contains(@class,'menu-dots')]")).Click();
+            WaitUntilVisibleAndClick(By.XPath(".//a[contains(text(),'" + Record1 + "')]" +
+                "//ancestor::tr//*[contains(@class,'shadow-menu ')]//*[contains(text(),'Delete')]"));
+            WaitUntilVisibleAndClick(By.XPath("//*[contains(@class,'body shadow-modal')]//button[contains(text(),'Delete')]"));
+            WaitUntilInVisible(By.XPath(".//a[contains(text(),'" + Record1 + "')]"));
+            Console.WriteLine(Message + Record1);
+
+            Message = "4. Delete the record on the page of record ";
+            driver.FindElement(By.XPath("(.//*[@role='rowgroup']//a)[contains(text(),'" + Record2 + "')]")).Click();
+            WaitUntilVisible(By.XPath("//button[contains(text(),'Delete')]"));
+            WaitUntilInVisible(By.XPath(".//*[@class='wrapper-preloader']/div"));//preloDER isnt
+            driver.FindElement(By.XPath("//button[contains(text(),'Delete')]")).Click();
+            WaitUntilVisibleAndClick(By.XPath("//*[contains(@class,'body shadow-modal')]//button[contains(text(),'Delete')]"));
+            WaitUntilVisible(By.XPath("//*[contains(text(),'Record " + Record2 + " deleted successfully')]"));
+            WaitUntilInVisible(By.XPath("(.//*[@role='rowgroup']//a)[contains(text(),'" + Record2 + "')]"));
+            Console.WriteLine(Message + Record2);
+
+        }
+
+
+        [Test]
+        [TestCaseSource(nameof(GetSites))]
+        public void City(string site)
+        {
+            string City = "CityAutoTest";
+            string EditCity = "EditCityAutoTest";
+            driver.Navigate().GoToUrl(baseURL + "cabinet/cities");
+            Authorization(site, AdminLogin);
+            Message = "2. Search through a filter the sity " + City;
+            WaitUntilVisible(By.XPath(".//*[@role='rowgroup']/tr[1]"));
+            WaitUntilVisible(By.XPath(".//*[@id='cabinet-search']"));//filter
+            driver.FindElement(By.XPath(".//*[@id='cabinet-search']")).SendKeys(City);
+            string Tolal = driver.FindElement(By.XPath(".//*[contains(text(),'Total')]")).Text;
+            driver.FindElement(By.XPath(".//*[@type='submit']")).Click();
+
+            //ждем прелоадер появится, исчезнет
+            WaitUntilVisibleInText(By.XPath(".//*[contains(text(),'Total')]"), Tolal);
+            Console.WriteLine(Message);
+            //если есть юзер, то удаляем
+            if (IsElementPresent(By.XPath(".//div[contains(text(),'" + City + "')]")) == true)
+            {
+                Message = "  2.1 Deleting " + City;
+                driver.FindElement(By.XPath(".//*[@role='cell']//*[contains(@class,'menu-dots')]")).Click();
+                WaitUntilVisibleAndClick(By.XPath(".//*[@role='cell']//*[contains(text(),'Delete')]"));
+                WaitUntilVisibleAndClick(By.XPath(".//*[contains(@class,'shadow-modal')]//*[contains(text(),'Delete')]"));
+                WaitUntilInVisible(By.XPath(".//a[contains(text(),'" + City + "')]"));
+                Console.WriteLine(Message);
+            }
+            //если есть editюзер, то удаляем
+            if (IsElementPresent(By.XPath(".//div[contains(text(),'" + EditCity + "')]")) == true)
+            {
+                Message = "Delete " + EditCity;
+                driver.FindElement(By.XPath(".//*[@role='cell']//*[contains(@class,'menu-dots')]")).Click();
+                WaitUntilVisibleAndClick(By.XPath(".//*[@role='cell']//*[contains(text(),'Delete')]"));
+                WaitUntilVisibleAndClick(By.XPath(".//*[contains(@class,'shadow-modal')]//*[contains(text(),'Delete')]"));
+                WaitUntilInVisible(By.XPath(".//a[contains(text(),'" + EditCity + "')]"));
+                Console.Write(Message);
+            }
+            Console.WriteLine("3. Adding the new City " + City);
+            Message = "  3.1 Field Validation";
+            WaitUntilVisibleAndClick(By.XPath(".//button[contains(text(),'Add city')]"));
+            WaitUntilVisible(By.XPath(".//*[@type='submit']"));
+            driver.FindElement(By.XPath(".//*[@type='submit']")).Click();
+            //if (IsElementPresent(By.XPath(".//*[@id='text']/following-sibling::span[contains(@class,'error')]")) == false)
+            //{
+            //    driver.FindElement(By.XPath(".//*[@type='submit']")).Click();
+            //}
+            WaitUntilVisible(By.XPath(".//*[@id='text']/following-sibling::span[contains(@class,'error')]"));
+            Console.WriteLine(Message);
+
+            Message = "  3.2 Entering data into fields ";
+            driver.FindElement(By.XPath(".//*[@id='text']")).SendKeys(City);
+            driver.FindElement(By.XPath(".//*[@type='submit']")).Click();
+            WaitUntilVisible(By.XPath(".//button[contains(text(),'Add city')]"));
+            driver.FindElement(By.XPath(".//div[contains(text(),'" + City + "')]"));
+            Console.WriteLine(Message);
+
+            //edit the user
+            Message = "4. User editing";
+            driver.FindElement(By.XPath(".//*[@role='cell']//*[contains(@class,'menu-dots')]")).Click();
+            WaitUntilVisibleAndClick(By.XPath(".//*[@role='cell']//*[contains(text(),'Edit')]"));
+            WaitUntilVisible(By.XPath(".//*[@id='text'][@value='"+ City + "']"));
+            driver.FindElement(By.XPath(".//*[@id='text']")).Clear();
+            driver.FindElement(By.XPath(".//*[@id='text']")).SendKeys(EditCity);
+            driver.FindElement(By.XPath(".//*[@type='submit']")).Click();
+            WaitUntilVisible(By.XPath(".//button[contains(text(),'Add city')]"));
+            WaitUntilVisible(By.XPath(".//div[contains(text(),'" + EditCity + "')]"));
+            Console.WriteLine(Message);
+
+            //deleting the user
+            Message = "5. Deleting city " + EditCity;
+            driver.FindElement(By.XPath(".//*[@role='cell']//*[contains(@class,'menu-dots')]")).Click();
+            WaitUntilVisibleAndClick(By.XPath(".//*[@role='cell']//*[contains(text(),'Delete')]"));
+            WaitUntilVisibleAndClick(By.XPath(".//*[contains(@class,'shadow-modal')]//*[contains(text(),'Delete')]"));
+            WaitUntilInVisible(By.XPath(".//div[contains(text(),'" + EditCity + "')]"));
+            Console.WriteLine(Message);
+        }
+
+
+
+
+
+
 
     }
 }
